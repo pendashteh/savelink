076fb3c5976312c2e93c35a0dcecaad25c6a7c69
Alexar <github@alexar.me>
2020-05-14 19:51:01 +1000

    Fixes an issue in date format and introduces a local directory folder.

--- savelink.sh
+++ savelink.sh
@@ -3,11 +3,13 @@ url=$1
 title=$(echo $url | sed -e 's#https\?://##g;s/www.//g;s/[^A-Za-z0-9._-]/_/g')
 read -p "Title? [$title]" name
 [ -z "$name" ] && name=$title
-name=$(date +%Y%d%m)"-"$name
+name=$(date +%Y%m%d)"-"$name
 
-path_default='.'
+path_default=$HOME/.savelink/data
+
 read -e -p "Location? [$path_default]" path
 [ -z "$path" ] && path=$path_default
+[ ! -d "$path" ] && mkdir -p $path
 
 if [ ! -d $path ]; then
   echo "Path not found."
@@ -18,7 +20,7 @@ mkdir -p $name
 cd $name
 wget $url -O output.html
 
-echo -e "URL:\t"$url"\nTime:\t"$(date +%Y-%m-%d %H:%M:%S)"\nFilesize:\t"$(stat --printf="%s" output.html) > info.txt
+echo -e "URL:\t"$url"\nTime:\t"$(date +%Y-%m-%d--%H:%M:%S)"\nFilesize:\t"$(stat --printf="%s" output.html) > info.txt
 
 echo "Your URL is saved: $url"
 
